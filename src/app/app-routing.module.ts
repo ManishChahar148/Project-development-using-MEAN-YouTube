5582383d17abb818c61a9c7954cac93cb0acd08b
unknown <ManishChahar148>
2020-02-29 23:02:30 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { FeaturesComponent } from './features/features.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { ShopComponent } from './shop/shop.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+{ path:'home',component:HomeComponent},
+{ path:'features', component : FeaturesComponent},
+{ path:'pricing', component:PricingComponent},
+{ path :'shop', component :ShopComponent},
+{path:'',component :HomeComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { FeaturesComponent } from './features/features.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { ShopComponent } from './shop/shop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FeaturesComponent,
+    PricingComponent,
+    ShopComponent
   ],
   imports: [
     BrowserModule,
